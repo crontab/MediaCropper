927ce33e9f99650abfdb4b701d79718b4f90bdcc
Hovik Melikyan <hovik.melikyan@gmail.com>
2020-12-02 16:38:31 +0100

    Created a workspace with demo + framework

---
+++ MediaCropper/MediaCropper.h
@@ -0,0 +1,18 @@
+//
+//  MediaCropper.h
+//  MediaCropper
+//
+//  Created by Hovik Melikyan on 02/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MediaCropper.
+FOUNDATION_EXPORT double MediaCropperVersionNumber;
+
+//! Project version string for MediaCropper.
+FOUNDATION_EXPORT const unsigned char MediaCropperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaCropper/PublicHeader.h>
+
+
